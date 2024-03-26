286c3b835b72056c5b5cc374b6a09131e7c8bcb5
Farid Lotfi <138003177+fariidlotfi@users.noreply.github.com>
2024-03-26 14:17:39 +0330

    fix: server

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 5173,
-    open: true,
-  },
   base: "/fm-rest-countries-api",
   plugins: [react()],
 });
