9548d138fdab06ca797625da987d00ca2a5aaee3
Farid Lotfi <138003177+fariidlotfi@users.noreply.github.com>
2024-03-26 17:07:18 +0330

    fix: pathname

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/fm-rest-countries-api",
+  base: "/fm-rest-countries-api",
   plugins: [react()],
 });
