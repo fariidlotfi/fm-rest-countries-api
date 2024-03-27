15b4fa086ce8ea41de042bd4b3e3fea6003d900b
Farid Lotfi <138003177+fariidlotfi@users.noreply.github.com>
2024-03-27 13:06:01 +0330

    fix: change routing method

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { routes } from "./routes";
 import Navbar from "./Components/Navbar";
+import Homepage from "./Pages/Homepage";
+import CountryDetail from "./Pages/CountryDetail";
 
 function App() {
   return (
     <>
       <Navbar />
-      <RouterProvider router={routes} />
+      <Router>
+        <Routes>
+          <Route exact path="/fm-rest-countries-api" element={<Homepage />} />
+          <Route
+            path="/fm-rest-countries-api/country/:countryName"
+            element={<CountryDetail />}
+          />
+        </Routes>
+      </Router>
     </>
   );
 }
